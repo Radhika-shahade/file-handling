870e806179c116339675199a56ca29c7ba25a33e
Radhika shahade <radhikashahade0804@gmail.com>
2024-01-03 18:04:13 +0530

    metrics of house

---
+++ src/main/java/org/example/file_handling/file_handling/house_file/metrics/BedroomMetrics.java
@@ -0,0 +1,15 @@
+package org.example.file_handling.file_handling.house_file.metrics;
+
+import org.example.file_handling.file_handling.house_file.model.House;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class BedroomMetrics implements MetricCollector{
+    @Override
+    public void collect(List<House> data) {
+        Map<Integer, List<House>> bedrooms = data.stream().collect(Collectors.groupingBy(House::getBedrooms));
+        bedrooms.forEach((k,v)-> System.out.println(k + " BHK "+ v.size()) );
+    }
+}
