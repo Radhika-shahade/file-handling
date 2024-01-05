b84a0b6f6cc7a79126e4d7c6966fd499e3a18bb2
Radhika shahade <radhikashahade0804@gmail.com>
2024-01-05 13:09:43 +0530

    Duplicate Finder

---
+++ src/main/java/org/example/file_handling/file_handling/house_file/duplicate/DuplicateFinder.java
@@ -0,0 +1,41 @@
+package org.example.file_handling.file_handling.house_file.duplicate;
+
+import org.example.file_handling.file_handling.house_file.model.House;
+
+import java.util.List;
+
+public class DuplicateFinder {
+    public static void findDuplicates(List<House> data){
+        data.forEach(d -> {
+            long count = duplicateCount(d, data);
+            if(count > 1){
+                System.out.println(count + ": " + d);
+            }
+        });
+        // Define House1 and House2
+    }
+
+    public static long duplicateCount(House house, List<House> data){
+        return data.stream().filter(h -> isDuplicate(house, h)).count();
+    }
+
+    public static boolean isDuplicate(House left, House right){
+        if (!left.getNeighbourhood().equals(right.getNeighbourhood())) {
+            return false;
+        }
+        if (left.getBedrooms() != right.getBedrooms()) {
+            return false;
+        }
+        if (left.getBathrooms() != right.getBathrooms()) {
+            return false;
+        }
+        if (left.getSquareFeet() != right.getSquareFeet()) {
+            return false;
+        }
+
+        if (left.getYear() != right.getYear()) {
+            return false;
+        }
+        return true;
+    }
+}
